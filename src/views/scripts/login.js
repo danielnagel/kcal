fed58086981c7fa8593aefc70a62401ae22b5438
Daniel Nagel <danielnagel@mailbox.org>
2024-11-06 21:08:56 +0100

    added login page

---
+++ src/views/scripts/login.js
@@ -0,0 +1,15 @@
+import { getFormDataJson } from "./utils.js";
+
+(() => {
+    window.onload = () => {
+        const form = document.getElementById('login-form');
+        form.onsubmit = async (e) => {
+            e.preventDefault();
+            const data = getFormDataJson(form);
+            console.log(data);
+            const credentials = btoa(`${data.username}:${data.password}`);
+            sessionStorage.setItem('userName', data.username);
+            sessionStorage.setItem('authToken', `Basic ${credentials}`);
+        };
+    }
+})()
