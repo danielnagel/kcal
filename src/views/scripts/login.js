fd3ab9306a25aed144448d8be674e61fcd8c6c9d
Daniel Nagel <danielnagel@mailbox.org>
2024-11-06 21:09:24 +0100

    lint

--- src/views/scripts/login.js
+++ src/views/scripts/login.js
@@ -1,15 +1,17 @@
-import { getFormDataJson } from "./utils.js";
+import {
+	getFormDataJson 
+} from './utils.js';
 
 (() => {
-    window.onload = () => {
-        const form = document.getElementById('login-form');
-        form.onsubmit = async (e) => {
-            e.preventDefault();
-            const data = getFormDataJson(form);
-            console.log(data);
-            const credentials = btoa(`${data.username}:${data.password}`);
-            sessionStorage.setItem('userName', data.username);
-            sessionStorage.setItem('authToken', `Basic ${credentials}`);
-        };
-    }
-})()
+	window.onload = () => {
+		const form = document.getElementById('login-form');
+		form.onsubmit = async (e) => {
+			e.preventDefault();
+			const data = getFormDataJson(form);
+			console.log(data);
+			const credentials = btoa(`${data.username}:${data.password}`);
+			sessionStorage.setItem('userName', data.username);
+			sessionStorage.setItem('authToken', `Basic ${credentials}`);
+		};
+	};
+})();
