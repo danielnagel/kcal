d8d11b2afa938cb2622f5409c2b93947cdb1ce99
Daniel Nagel <danielnagel@mailbox.org>
2024-11-13 07:08:58 +0100

    removed offline capabilities

--- src/views/scripts/input_kcal.js
+++ src/views/scripts/input_kcal.js
@@ -1,13 +1,10 @@
 import {
 	bootstrapApp,
-	copyToClipboard,
 	getFormDataJson,
 	getSession,
 	infoAlert,
 	errorAlert
 } from './utils.js';
-
-const storageItemKey = 'unsendFormData';
 
 const updateDateTimeInput = () => {
 	const datetimeInput = document.querySelector('input[type=datetime-local]');
@@ -40,21 +37,6 @@ const renderSuggestionList = (data) => {
 	});
 };
 
-const getParsedStoredData = () => {
-	const result = [];
-	if (!localStorage) return [];
-	const storedData = localStorage.getItem(storageItemKey);
-	if (storedData) {
-		try {
-			result.push(...JSON.parse(storedData));
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		} catch (e) {
-			console.error(`could not parse data from store. stored data: ${storedData}`);
-		}
-	}
-	return result;
-};
-
 const sendDataList = async (dataList, userName, authToken) => {
 	const response = await fetch(`/api/input_kcal?user=${userName}`, {
 		method: 'POST',
@@ -65,15 +47,11 @@ const sendDataList = async (dataList, userName, authToken) => {
 		},
 	});
 	if (response.status == 404) {
-		if (localStorage) localStorage.setItem(storageItemKey, JSON.stringify(dataList));
 		errorAlert('There is no connection to the server.');
-		renderOfflineInfo(userName);
 	} else if (response.status === 500) {
 		const data = await response.json();
 		errorAlert(data.message);
 	} else {
-		if (localStorage)
-			localStorage.removeItem(storageItemKey);
 		const form = document.getElementById('kcal-form');
 		form.reset();
 		updateDateTimeInput();
@@ -85,45 +63,8 @@ const formHandling = (userName, authToken) => {
 	const form = document.getElementById('kcal-form');
 	form.onsubmit = async (e) => {
 		e.preventDefault();
-		sendDataList([...getParsedStoredData(), getFormDataJson(form)], userName, authToken);
+		sendDataList([getFormDataJson(form)], userName, authToken);
 	};
-};
-
-const showOfflineContainer = () => {
-	const offlineContainer = document.getElementById('offline-container');
-	if (offlineContainer.classList.contains('hidden')) offlineContainer.classList.remove('hidden');
-};
-
-const hideOfflineContainer = () => {
-	const offlineContainer = document.getElementById('offline-container');
-	if (!offlineContainer.classList.contains('hidden')) offlineContainer.classList.add('hidden');
-};
-
-const renderOfflineInfo = (userName) => {
-	if (localStorage) {
-		const storedData = localStorage.getItem(storageItemKey);
-		if (storedData) {
-			const data = [];
-			try {
-				data.push(...JSON.parse(storedData));
-				// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			} catch (e) {
-				console.error(`could not parse data from store. stored data: ${storedData}`);
-			}
-			const offlineMessage = document.getElementById('offline-message');
-			const copyButton = document.getElementById('offline-copy-button');
-			const sendButton = document.getElementById('offline-resend-button');
-			offlineMessage.innerText = `Data could not be send, ${data.length} stored items.`;
-			copyButton.onclick = () => copyToClipboard(storedData);
-			sendButton.onclick = async () => {
-				await sendDataList(getParsedStoredData(), userName);
-				if (localStorage && !localStorage.getItem(storageItemKey)) hideOfflineContainer();
-			};
-			showOfflineContainer();
-			return;
-		}
-	}
-	hideOfflineContainer();
 };
 
 const getAndRenderSuggestionList = async (userName, authToken) => {
@@ -148,7 +89,6 @@ const getAndRenderSuggestionList = async (userName, authToken) => {
 		if (userName && authToken) {
 			await getAndRenderSuggestionList(userName, authToken);
 			formHandling(userName, authToken);
-			renderOfflineInfo(userName);
 		}
 	};
 })();
--- src/views/scripts/utils.js
+++ src/views/scripts/utils.js
@@ -1,28 +1,3 @@
-const unsecuredCopyToClipboard = (text) => {
-	const textArea = document.createElement('textarea');
-	textArea.value = text;
-	document.body.appendChild(textArea);
-	textArea.focus();
-	textArea.select();
-	try {
-		document.execCommand('copy');
-	} catch (err) {
-		console.error('Unable to copy to clipboard', err);
-	}
-	document.body.removeChild(textArea);
-};
-
-export const copyToClipboard = (text) => {
-	if (navigator.clipboard) {
-		// If normal copy method available, use it
-		navigator.clipboard.writeText(text);
-	} else {
-		// Otherwise fallback to the above function, e.g. iOS using http
-		// https://stackoverflow.com/a/72239825
-		unsecuredCopyToClipboard(text);
-	}
-};
-
 const installServiceWorker = () => {
 	if (navigator.serviceWorker) {
 		window.addEventListener('load', () => {
